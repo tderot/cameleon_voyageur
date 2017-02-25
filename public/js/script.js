fa05bd883551de2d20452bfc8ff632528abe9fdb
Quentin RIANDIERE <quentin.riandiere@gmail.com>
2017-02-25 19:03:33 +0100

    Une barre de nav un peu plus belle, mise en place de la photo de couverture

--- public/js/script.js
+++ public/js/script.js
@@ -3,8 +3,10 @@ $(function() {
         var scroll = $(window).scrollTop();
         if (scroll >= 20){
             $('#navbar').addClass('navbar-fixed-top');
+            $('#navbar').addClass('navbar_color');
 		}else{
             $('#navbar').removeClass('navbar-fixed-top');
+            $('#navbar').removeClass('navbar_color');
 		}
 	});
 });
